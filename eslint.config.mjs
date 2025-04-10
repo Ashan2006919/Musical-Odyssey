416064f311650765638276f8df8652b53451ba99
Ashan2006919 <E210361@esoft.academy>
2025-04-10 13:04:13 +0530

    Fixed a lot

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    parser: "@babel/eslint-parser", // Use Babel parser
+    parserOptions: {
+      requireConfigFile: false, // Allow parsing without a Babel config file
+    },
+  },
+];
 
 export default eslintConfig;
